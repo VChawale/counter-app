150133b17ed98d1c6a02d23f0a29b2963b1e5a54
VChawale <vaibhavwchawale@gmail.com>
2021-09-21 21:16:22 -0500

    lesson-9- conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,39 +3,28 @@ import React, { Component } from "react";
 class Counter extends React.Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
-  //   styles = {
-  //     fontSize: 10,
-  //     fontWeight: "bold",
-  //   };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags available</p>;
 
-  render() {
     return (
-      <React.Fragment>
-        {/* <span style={{ fontSize: 50 }} className="badge badge-primary m-2"> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Incement</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please enter a new tag"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
